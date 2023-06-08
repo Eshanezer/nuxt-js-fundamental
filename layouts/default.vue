918f7e1a9f0ab50babd18c36bfd92b80f3b1daa9
Eshan Dilmina <106302589+eshanceylonsoft@users.noreply.github.com>
2023-06-08 19:19:22 +0530

    implemented

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,31 +1,23 @@
 <template>
-    <header>
-        <nav>
-            <ul>
-                <li>
-                    <nuxt-link to="/">Nuxt JS</nuxt-link>
-                </li>
-                <li>
-                    <nuxt-link to="/about">About</nuxt-link>
-                </li>
-                <li>
-                    <nuxt-link to="/products">Products</nuxt-link>
-                </li>
-            </ul>
-        </nav>
-    </header>
-
-    <!-- page content -->
-
     <div>
-        <slot/>
+      <header class="shadow-sm bg-white">
+        <nav class="container mx-auto p-4 flex justify-between">
+          <NuxtLink to="/" class="font-bold">Nuxt Dojo</NuxtLink>
+          <ul class="flex gap-4">
+            <li><NuxtLink to="/">Home</NuxtLink></li>
+            <li><NuxtLink to="/about">About</NuxtLink></li>
+            <li><NuxtLink to="/products" class="btn">Products</NuxtLink></li>
+          </ul>
+        </nav>
+      </header>
+      <div class="container mx-auto p-4">
+        <slot />
+      </div>
     </div>
-</template>
-
-<script setup>
-
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+  </template>
+  
+  <style scoped>
+    .router-link-exact-active {
+      color: #12b488;
+    }
+  </style>
--- layouts/products.vue
+++ layouts/products.vue
@@ -1,34 +1,25 @@
 <template>
-    <header>
-        <nav>
-            <ul>
-                <li>
-                    <nuxt-link to="/">Nuxt JS</nuxt-link>
-                </li>
-            </ul>
-        </nav>
-    </header>
-
-    <!-- page content -->
-
     <div>
-        <slot/>
+      <header class="shadow-sm bg-white">
+        <nav class="container mx-auto p-4">
+          <NuxtLink to="/products" class="font-bold">Nuxt Dojo Merch</NuxtLink>
+        </nav>
+      </header>
+      <div class="container mx-auto p-4">
+        <slot />
+      </div>
+      <footer class="container mx-auto p-4 flex justify-between border-t-2">
+        <ul class="flex gap-4">
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/products">Products</NuxtLink></li>
+        </ul>
+      </footer>
     </div>
-
-    <footer>
-        <li>
-                    <nuxt-link to="/about">About</nuxt-link>
-                </li>
-                <li>
-                    <nuxt-link to="/products">Products</nuxt-link>
-                </li>
-    </footer>
-</template>
-
-<script setup>
-
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+  </template>
+  
+  <style scoped>
+    .router-link-exact-active {
+      color: #12b488;
+    }
+  </style>
